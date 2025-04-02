1da4b33891d9f29b00cefa149fe73f67dc618cc9
Nico Clari <clarinicolas7@gmail.com>
2025-04-02 13:20:42 -0300

    Update principal.ino

--- principal/principal.ino
+++ principal/principal.ino
@@ -6,10 +6,17 @@
 
 */
 // defines pins numbers
+const int LED = 2;
+const int LED2 = 3;
 const int trigPin = 9;
 const int echoPin = 10;
 const int trigPin2 = 11;
 const int echoPin2 = 12;
+
+const int in1 = 5;
+const int in2 = 6;
+const int in3 = 7;
+const int in4 = 8;
 // defines variables
 long duration;
 int distance;
@@ -21,38 +28,54 @@ void setup() {
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   pinMode(trigPin2, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin2, INPUT); // Sets the echoPin as an Input
+
+  pinMode(in1, OUTPUT); // Sets the trigPin as an Output
+  pinMode(in2, OUTPUT); // Sets the trigPin as an Output
+  pinMode(in3, OUTPUT); // Sets the trigPin as an Output
+  pinMode(in4, OUTPUT); // Sets the trigPin as an Output
+
   Serial.begin(9600); // Starts the serial communication
 }
 void loop() {
   // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
+
+  distance = ultrasonic(trigPin,echoPin)
   Serial.print("Distance: ");
   Serial.println(distance);
 
-    // Clears the trigPin
-  digitalWrite(trigPin2, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin2, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin2, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration2 = pulseIn(echoPin2, HIGH);
-  // Calculating the distance
-  distance2 = duration2 * 0.034 / 2;
-  // Prints the distance on the Serial Monitor
+  
+  distance2 = ultrasonic(trigPin2,echoPin2)
   Serial.print("Distance2: ");
   Serial.println(distance2);
 
+  if (distance==20){
+    frente();
+  }
+
 }
+
+int ultrasonico(int tPin, int ePin){
+    // Clears the trigPin
+    int d;
+  digitalWrite(tPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(tPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(tPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(ePin, HIGH);
+  // Calculating the distance
+  d = duration * 0.034 / 2;
+  return d;
+}
+
+void frente (    ){
+  digitalWrite(in1,HIGH);
+  digitalWrite(in2,LOW);
+  digitalWrite(in3,HIGH);  
+  digitalWrite(in4,LOW);
+}
+
+
  
